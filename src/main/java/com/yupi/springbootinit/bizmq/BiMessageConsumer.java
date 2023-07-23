a7e4415a99e29c0e216421867a076734f669fd27
hxq <1483608301@qq.com>
2023-07-23 21:41:45 +0800

    增加guava Retrying重试机制

--- src/main/java/com/yupi/springbootinit/bizmq/BiMessageConsumer.java
+++ src/main/java/com/yupi/springbootinit/bizmq/BiMessageConsumer.java
@@ -1,5 +1,7 @@
 package com.yupi.springbootinit.bizmq;
 
+import com.github.rholder.retry.*;
+import com.google.common.base.Predicates;
 import com.rabbitmq.client.Channel;
 import com.yupi.springbootinit.common.ErrorCode;
 import com.yupi.springbootinit.constant.CommonConstant;
@@ -17,6 +19,10 @@ import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
+import java.io.IOException;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
 
 @Component
 @Slf4j
@@ -45,7 +51,7 @@ public class BiMessageConsumer {
             throw new BusinessException(ErrorCode.PARAMS_ERROR, "图表为空");
         }
         StatusEnum initStatus = chart.getStatus();
-        if(initStatus==StatusEnum.wait) {
+        if (initStatus == StatusEnum.wait) {
             Chart updateChart = new Chart();
             updateChart.setId(chart.getId());
             updateChart.setStatus(StatusEnum.running);
@@ -57,44 +63,77 @@ public class BiMessageConsumer {
             }
         }
         //调用AI
-        String result = null;
+//        String result = null;
+//        try {
+//            result = aiManager.doChat(CommonConstant.MODEL_ID, buildUserInput(chart));
+//        } catch (Exception e) {
+//            handleChartUpdate(chart.getId(), e.getMessage(), StatusEnum.retry);
+//            if (redeliveryCount < 4) {
+//                chanenel.basicNack(deliveryTag, false, true);
+//                log.info("重试次数：{}", redeliveryCount);
+//                redeliveryCount++;
+//                Thread.sleep(30000);
+//                return;
+//            }
+//            redeliveryCount = 1;
+//            handleChartUpdate(chart.getId(), e.getMessage(), StatusEnum.failed);
+//            chanenel.basicReject(deliveryTag, false);
+//        }
+        Callable<String> callable = () -> {
+            return aiManager.doChat(CommonConstant.MODEL_ID, buildUserInput(chart));
+        };
+
+        Long param = chart.getId(); // 这里是示例参数，你需要根据自己的逻辑来设置
+
+        // 定义重试器
+        Retryer<String> retryer = RetryerBuilder.<String>newBuilder()
+                .retryIfResult(Predicates.<String>isNull()) // 如果结果为空则重试
+                .retryIfExceptionOfType(IOException.class) // 发生IO异常则重试
+                .retryIfRuntimeException() // 发生运行时异常则重试
+                .withWaitStrategy(WaitStrategies.incrementingWait(6, TimeUnit.SECONDS, 18, TimeUnit.SECONDS)) // 等待
+                .withStopStrategy(StopStrategies.stopAfterAttempt(3)) // 允许执行3次（首次执行 + 最多重试2次）
+                .withRetryListener(new RetryListener() {
+                    private final Long chartId = param; // 在匿名内部类中引用外部参数
+                    @Override
+                    public <V> void onRetry(Attempt<V> attempt) {
+                       handleChartUpdate(chartId,"Ai生成错误",StatusEnum.retry);
+                    }
+                })
+                .build();
         try {
-            result = aiManager.doChat(CommonConstant.MODEL_ID, buildUserInput(chart));
-        } catch (Exception e) {
-            handleChartUpdate(chart.getId(), e.getMessage(), StatusEnum.retry);
-            if (redeliveryCount < 4) {
-                chanenel.basicNack(deliveryTag, false, true);
-                log.info("重试次数：{}", redeliveryCount);
-                redeliveryCount++;
-                Thread.sleep(30000);
-                return;
+            String result=retryer.call(callable); // 执行
+            if (result != null && !result.isEmpty()){
+                String[] splits = result.split("【【【【");
+                if (splits.length < 3) {
+                    chanenel.basicNack(deliveryTag, false, false);
+                    handleChartUpdate(chart.getId(), "AI生成错误", StatusEnum.failed);
+                    throw new BusinessException(ErrorCode.SYSTEM_ERROR, "AI 生成错误");
+                }
+                String genChart = splits[1].trim();
+                String genResult = splits[2].trim();
+                Chart updateChartResult = new Chart();
+                updateChartResult.setId(chart.getId());
+                updateChartResult.setGenChart(genChart);
+                updateChartResult.setGenResult(genResult);
+                updateChartResult.setStatus(StatusEnum.succeed);
+                boolean updateResult = chartService.updateById(updateChartResult);
+                if (!updateResult) {
+                    chanenel.basicNack(deliveryTag, false, false);
+                    handleChartUpdate(chart.getId(), "更新图表成功状态失败", StatusEnum.failed);
+                    throw new BusinessException(ErrorCode.SYSTEM_ERROR, "更新图表成功状态失败");
+                }
+                //手动确认
+                chanenel.basicAck(deliveryTag, false);
+                handleChartUpdate(chart.getId(), "", StatusEnum.succeed);
             }
-            redeliveryCount = 1;
+        } catch (RetryException e) { // 重试次数超过阈值或被强制中断
+            handleChartUpdate(chart.getId(), "3次重试后，图表生成失败！！", StatusEnum.failed);
+            chanenel.basicReject(deliveryTag,false);
+        } catch (ExecutionException e) { // 重试次数超过阈值或被强制中断
             handleChartUpdate(chart.getId(), e.getMessage(), StatusEnum.failed);
-            chanenel.basicReject(deliveryTag, false);
+            chanenel.basicReject(deliveryTag,false);
         }
-        String[] splits = result.split("【【【【");
-        if (splits.length < 3) {
-            chanenel.basicNack(deliveryTag, false, true);
-            handleChartUpdate(chart.getId(), "AI生成错误", StatusEnum.failed);
-            throw new BusinessException(ErrorCode.SYSTEM_ERROR, "AI 生成错误");
-        }
-        String genChart = splits[1].trim();
-        String genResult = splits[2].trim();
-        Chart updateChartResult = new Chart();
-        updateChartResult.setId(chart.getId());
-        updateChartResult.setGenChart(genChart);
-        updateChartResult.setGenResult(genResult);
-        updateChartResult.setStatus(StatusEnum.succeed);
-        boolean updateResult = chartService.updateById(updateChartResult);
-        if (!updateResult) {
-            chanenel.basicNack(deliveryTag, false, false);
-            handleChartUpdate(chart.getId(), "更新图表成功状态失败", StatusEnum.failed);
-            throw new BusinessException(ErrorCode.SYSTEM_ERROR, "更新图表成功状态失败");
-        }
-        //手动确认
-        chanenel.basicAck(deliveryTag, false);
-        handleChartUpdate(chart.getId(), "", StatusEnum.succeed);
+
     }
 
 
--- src/main/java/com/yupi/springbootinit/bizmq/BiinitMain.java
+++ src/main/java/com/yupi/springbootinit/bizmq/BiinitMain.java
@@ -4,6 +4,9 @@ import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class BiinitMain {
     public static void main(String[] args) {
         try {
@@ -14,12 +17,12 @@ public class BiinitMain {
             String EXCHANGE_NAME = BiConstant.BI_EXCHANGE_NAME;
             //声明交换机
             channel.exchangeDeclare(EXCHANGE_NAME, "direct");
-//            Map<String, Object> arg= new HashMap<String, Object>();
-//            arg.put("x-message-ttl", 100000);
+            Map<String, Object> arg= new HashMap<String, Object>();
+            arg.put("x-message-ttl", 100000);
             String queueName = BiConstant.BI_QUEUE_NAME;
             //创建队列
             channel.basicQos(2);
-            channel.queueDeclare(queueName, true, false, false, null);
+            channel.queueDeclare(queueName, true, false, false, arg);
             channel.queueBind(queueName, EXCHANGE_NAME, BiConstant.BI_ROUTING_KEY);
         } catch (Exception e) {
             throw new RuntimeException(e);
